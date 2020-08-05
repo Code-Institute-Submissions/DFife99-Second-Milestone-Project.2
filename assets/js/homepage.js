fb23bff9f449da29026ec9897e708e96d5d1124b
no-name <devlan.fife2@gmail.com>
2020-08-05 07:56:04 +0100

    adding some text to some of the planets and moons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -162,9 +162,7 @@ $(document).on('click', '.planet-5-moon-carousel-1', function() {
     $('#planet-5-moon-3').css('display', 'none');
     $('#planet-5-moon-4').css('display', 'none');
     $('#planet-5-moon-5').css('display', 'none');
-    $('#planet-5-moon-6').css('display', 'none');
-    $('#planet-5-moon-7').css('display', 'none');
-    $('#planet-5-moon-8').css('display', 'none');
+
 })
 
 $(document).on('click', '.planet-5-moon-carousel-2', function() {
@@ -173,9 +171,6 @@ $(document).on('click', '.planet-5-moon-carousel-2', function() {
     $('#planet-5-moon-3').css('display', 'none');
     $('#planet-5-moon-4').css('display', 'none');
     $('#planet-5-moon-5').css('display', 'none');
-    $('#planet-5-moon-6').css('display', 'none');
-    $('#planet-5-moon-7').css('display', 'none');
-    $('#planet-5-moon-8').css('display', 'none');
 })
 
 $(document).on('click', '.planet-5-moon-carousel-3', function() {
@@ -184,9 +179,6 @@ $(document).on('click', '.planet-5-moon-carousel-3', function() {
     $('#planet-5-moon-3').css('display', 'block');
     $('#planet-5-moon-4').css('display', 'none');
     $('#planet-5-moon-5').css('display', 'none');
-    $('#planet-5-moon-6').css('display', 'none');
-    $('#planet-5-moon-7').css('display', 'none');
-    $('#planet-5-moon-8').css('display', 'none');
 })
 
 $(document).on('click', '.planet-5-moon-carousel-4', function() {
@@ -195,9 +187,6 @@ $(document).on('click', '.planet-5-moon-carousel-4', function() {
     $('#planet-5-moon-3').css('display', 'none');
     $('#planet-5-moon-4').css('display', 'block');
     $('#planet-5-moon-5').css('display', 'none');
-    $('#planet-5-moon-6').css('display', 'none');
-    $('#planet-5-moon-7').css('display', 'none');
-    $('#planet-5-moon-8').css('display', 'none');
 })
 
 $(document).on('click', '.planet-5-moon-carousel-5', function() {
@@ -206,43 +195,9 @@ $(document).on('click', '.planet-5-moon-carousel-5', function() {
     $('#planet-5-moon-3').css('display', 'none');
     $('#planet-5-moon-4').css('display', 'none');
     $('#planet-5-moon-5').css('display', 'block');
-    $('#planet-5-moon-6').css('display', 'none');
-    $('#planet-5-moon-7').css('display', 'none');
-    $('#planet-5-moon-8').css('display', 'none');
 })
 
-$(document).on('click', '.planet-5-moon-carousel-6', function() {
-    $('#planet-5-moon-1').css('display', 'none');
-    $('#planet-5-moon-2').css('display', 'block');
-    $('#planet-5-moon-3').css('display', 'none');
-    $('#planet-5-moon-4').css('display', 'none');
-    $('#planet-5-moon-5').css('display', 'none');
-    $('#planet-5-moon-6').css('display', 'block');
-    $('#planet-5-moon-7').css('display', 'none');
-    $('#planet-5-moon-8').css('display', 'none');
-})
 
-$(document).on('click', '.planet-5-moon-carousel-7', function() {
-    $('#planet-5-moon-1').css('display', 'none');
-    $('#planet-5-moon-2').css('display', 'none');
-    $('#planet-5-moon-3').css('display', 'none');
-    $('#planet-5-moon-4').css('display', 'none');
-    $('#planet-5-moon-5').css('display', 'none');
-    $('#planet-5-moon-6').css('display', 'none');
-    $('#planet-5-moon-7').css('display', 'block');
-    $('#planet-5-moon-8').css('display', 'none');
-})
-
-$(document).on('click', '.planet-5-moon-carousel-8', function() {
-    $('#planet-5-moon-1').css('display', 'none');
-    $('#planet-5-moon-2').css('display', 'none');
-    $('#planet-5-moon-3').css('display', 'none');
-    $('#planet-5-moon-4').css('display', 'none');
-    $('#planet-5-moon-5').css('display', 'none');
-    $('#planet-5-moon-6').css('display', 'none');
-    $('#planet-5-moon-7').css('display', 'none');
-    $('#planet-5-moon-8').css('display', 'block');
-})
 
 // SATURN
 $(document).on('click', '.planet-carousel-6', function() {
@@ -282,6 +237,7 @@ $(document).on('click', '.planet-carousel-7', function() {
     $('#planet-5-moon').css('display', 'none');
     $('#planet-6-moon').css('display', 'none');
     $('#planet-7-moon').css('display', 'block');
+    $('#planet-7-moon-1').css('display', 'block');
     $('#planet-8-moon').css('display', 'none');
 })
 
