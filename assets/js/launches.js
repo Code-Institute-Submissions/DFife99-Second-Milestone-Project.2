25226e759e31cf1c117fdc929a2f44647599b57a
no-name <devlan.fife2@gmail.com>
2020-08-11 23:07:50 +0100

    playing around with different  background images to display for api when a set property is met

--- assets/js/launches.js
+++ assets/js/launches.js
@@ -5,6 +5,15 @@ $(document).ready(function() {
             console.log(element);
             let unixDate = new Date(element.launch_date_unix * 1000);
             let now = new Date().getTime();
+            if (element.launch_success === null) {
+                $('#launches').prepend('<div id="spacex-api-' + index + '" class="building"></div>')
+            } else {
+                if (element.launch_success === false) {
+                    $('#launches').prepend('<div id="spacex-api-' + index + '" class="failure"></div>')
+                } else {
+                    $('#launches').prepend('<div id="spacex-api-' + index + '" class="satelite"></div>')
+                }
+            }
             if (unixDate > now) {
 
                 // Find the distance between now and the count down date
@@ -19,10 +28,10 @@ $(document).ready(function() {
                 // Display the result in the element with id="demo"
                 var countdown = days + "d " + hours + "h " +
                     minutes + "m " + seconds + "s ";
-                $('#launches').prepend('<p><h3> Mission Name : ' + element.mission_name + '</h3><h4> Rocket Name : ' + element.rocket.rocket_id + '<br> Payload ID : ' + element.rocket.second_stage.payloads[0].payload_id + '<br> Payload Type : ' + element.rocket.second_stage.payloads[0].payload_type + '<br> Nationality : ' + element.rocket.second_stage.payloads[0].nationality + '<br>Launch Date : ' + countdown + '</h4></p>')
+                $('#spacex-api-' + index).prepend('<p><h3> Mission Name : ' + element.mission_name + '</h3><h4> Rocket Name : ' + element.rocket.rocket_id + '<br> Payload ID : ' + element.rocket.second_stage.payloads[0].payload_id + '<br> Payload Type : ' + element.rocket.second_stage.payloads[0].payload_type + '<br> Nationality : ' + element.rocket.second_stage.payloads[0].nationality + '<br>Launches In : ' + countdown + '</h4></p>')
 
             } else {
-                $('#launches').prepend('<p><h3> Mission Name : ' + element.mission_name + '</h3><h4> Rocket Name : ' + element.rocket.rocket_id + '<br> Payload ID : ' + element.rocket.second_stage.payloads[0].payload_id + '<br> Payload Type : ' + element.rocket.second_stage.payloads[0].payload_type + '<br> Nationality : ' + element.rocket.second_stage.payloads[0].nationality + '<br>Launch Date : ' + unixDate + '</h4></p>')
+                $('#spacex-api-' + index).prepend('<p><h3> Mission Name : ' + element.mission_name + '</h3><h4> Rocket Name : ' + element.rocket.rocket_id + '<br> Payload ID : ' + element.rocket.second_stage.payloads[0].payload_id + '<br> Payload Type : ' + element.rocket.second_stage.payloads[0].payload_type + '<br> Nationality : ' + element.rocket.second_stage.payloads[0].nationality + '<br>Launch Date : ' + unixDate + '<br><br> Details : ' + element.details + '</h4></p>')
             }
         }
     });
